803a0a4b6cd7f23e55590d1ba8a641ccb7dd71b5
Mario-Chavez <marioachavez1@gmail.com>
2023-08-10 17:00:59 -0300

    se debe dar estilos a la card de detalles

--- src/components/Detail.jsx
+++ src/components/Detail.jsx
@@ -2,14 +2,14 @@ import React from "react";
 import photo from "../assets/img-foto.jpg";
 import { useParams } from "react-router-dom";
 import { useGetOneFoodQuery } from "../api/apiSlice";
-import { Card } from "react-bootstrap";
+import { Card, Col, Row } from "react-bootstrap";
 import { FallingLines } from "react-loader-spinner";
 
 const Detail = () => {
     const params = useParams();
 
     const { data: food, isLoading, isError } = useGetOneFoodQuery(params.id);
-
+    console.log(food);
     return (
         <>
             {isLoading ? (
@@ -20,16 +20,31 @@ const Detail = () => {
                     ariaLabel="falling-lines-loading"
                 />
             ) : (
-                <Card border="primary" style={{ width: "18rem" }}>
-                    <Card.Header>Header</Card.Header>
+                <Card className="mx-auto my-5">
                     <Card.Body>
-                        <Card.Title>Primary Card Title</Card.Title>
-                        <Card.Text>
-                            Some quick example text to build on the card title and make up
-                            the bulk of the card's content.
-                        </Card.Text>
+                        <Row>
+                            <Col md={12}>
+                                <Card.Img src={food.imagen} />
+                            </Col>
+                            <Col md={5}>
+                                <Card.Title>{food.nombre}</Card.Title>
+                                <Card.Text>{food.categoria}</Card.Text>
+                            </Col>
+                        </Row>
                     </Card.Body>
                 </Card>
+
+                // <Card border="primary" style={{ width: "18rem" }}>
+                //     <Card.Header>{food.nombre}</Card.Header>
+                //     <Card.Img variant="top" src={food.imagen} />
+                //     <Card.Body>
+                //         <Card.Title>{food.nombre}</Card.Title>
+                //         <Card.Text>
+                //             Some quick example text to build on the card title and make up
+                //             the bulk of the card's content.
+                //         </Card.Text>
+                //     </Card.Body>
+                // </Card>
             )}
         </>
     );
--- src/pages/DetailPage.jsx
+++ src/pages/DetailPage.jsx
@@ -1,4 +1,5 @@
 import React from "react";
+import Detail from "../components/Detail";
 
 const DetailPage = () => {
     return (
