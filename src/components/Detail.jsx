706cc5c83d07d22fb5ca138d78f7360d0fb4bc19
Mario-Chavez <marioachavez1@gmail.com>
2023-08-10 11:40:11 -0300

    se ca,bio el estilos de las targetas

--- src/components/Detail.jsx
+++ src/components/Detail.jsx
@@ -3,6 +3,7 @@ import photo from "../assets/img-foto.jpg";
 import { useParams } from "react-router-dom";
 import { useGetOneFoodQuery } from "../api/apiSlice";
 import { Card } from "react-bootstrap";
+import { FallingLines } from "react-loader-spinner";
 
 const Detail = () => {
     const params = useParams();
@@ -11,16 +12,25 @@ const Detail = () => {
 
     return (
         <>
-            <Card border="primary" style={{ width: "18rem" }}>
-                <Card.Header>Header</Card.Header>
-                <Card.Body>
-                    <Card.Title>Primary Card Title</Card.Title>
-                    <Card.Text>
-                        Some quick example text to build on the card title and make up the
-                        bulk of the card's content.
-                    </Card.Text>
-                </Card.Body>
-            </Card>
+            {isLoading ? (
+                <FallingLines
+                    color="#4fa94d"
+                    width="100"
+                    visible={true}
+                    ariaLabel="falling-lines-loading"
+                />
+            ) : (
+                <Card border="primary" style={{ width: "18rem" }}>
+                    <Card.Header>Header</Card.Header>
+                    <Card.Body>
+                        <Card.Title>Primary Card Title</Card.Title>
+                        <Card.Text>
+                            Some quick example text to build on the card title and make up
+                            the bulk of the card's content.
+                        </Card.Text>
+                    </Card.Body>
+                </Card>
+            )}
         </>
     );
 };
--- src/components/ListFoods.jsx
+++ src/components/ListFoods.jsx
@@ -1,7 +1,10 @@
 import React from "react";
 import { useGetFoodsQuery } from "../api/apiSlice";
-
+import { RotatingLines } from "react-loader-spinner";
 import { useNavigate } from "react-router-dom";
+import Placeholder from "react-bootstrap/Placeholder";
+import { Button, Card } from "react-bootstrap";
+
 const ListFoods = () => {
     const { data: food, isLoading, isError } = useGetFoodsQuery();
     const navigate = useNavigate();
@@ -12,34 +15,87 @@ const ListFoods = () => {
 
     return (
         <>
+            {/* 
+         <RotatingLines
+                            strokeColor="grey"
+                            strokeWidth="5"
+                            animationDuration="0.75"
+                            width="96"
+                            visible={true}
+                        />
+        */}
             {isLoading ? (
-                <div>loading...</div>
+                <h1>cargando</h1>
             ) : (
-                <div className="container">
-                    <div className="row">
-                        {food.map((food) => (
-                            <div key={food._id} className="col-md-3 my-2 ">
-                                <div className="card p-2">
-                                    <img src={food.imagen} alt="imagen" />
+                // <div className="container">
+                //     <div className="row">
+                //         {food.map((food) => (
+                //             <div key={food._id} className="col-md-3 my-2 ">
+                //                 <div className="card p-2">
+                //                     <img src={food.imagen} alt="imagen" />
 
-                                    <div className="card-body">
-                                        <h5 className="card-title">{`${food.nombre}`}</h5>
-                                        <h5 className="card-text">{`${food.categoria}`}</h5>
+                //                     <div className="card-body">
+                //                         <h5 className="card-title">{`${food.nombre}`}</h5>
+                //                         <h5 className="card-text">{`${food.categoria}`}</h5>
+                //                     </div>
+                //                     <button
+                //                         className="btn btn-info my-2"
+                //                         onClick={() => handleDetail(food._id)}
+                //                     >
+                //                         detalle
+                //                     </button>
+                //                     <button
+                //                         className="btn btn-danger my-2"
+                //                         onClick={() => handleDelet(user.id)}
+                //                     >
+                //                         eliminar
+                //                     </button>
+                //                 </div>
+                //             </div>
+                //         ))}
+                //     </div>
+                // </div>
+                <div className="container">
+                    <div className="row ">
+                        {food.map((food) => (
+                            <Card
+                                style={{ width: "18rem" }}
+                                className="col-md-3 my-2 mx-2"
+                            >
+                                <Card.Img variant="top" src={food.imagen} />
+                                <Card.Body>
+                                    <Card.Title>{`${food.nombre}`}</Card.Title>
+                                    <Card.Text>{`${food.categoria}`}</Card.Text>
+                                </Card.Body>
+                                <Card.Footer>
+                                    <div className="d-flex justify-content-between">
+                                        <Button variant="primary">Detail</Button>
+                                        <Button variant="danger">Delet</Button>
                                     </div>
-                                    <button
-                                        className="btn btn-info my-2"
-                                        onClick={() => handleDetail(food._id)}
-                                    >
-                                        detalle
-                                    </button>
-                                    <button
-                                        className="btn btn-danger my-2"
-                                        onClick={() => handleDelet(user.id)}
-                                    >
-                                        eliminar
-                                    </button>
-                                </div>
-                            </div>
+                                </Card.Footer>
+                            </Card>
+                            // <div key={food._id} >
+                            //     <div className="card p-2">
+                            //         <img src={food.imagen} alt="imagen" />
+
+                            //         <div className="card-body">
+                            //             <h5 className="card-title">{`${food.nombre}`}</h5>
+                            //             <h5 className="card-text">{`${food.categoria}`}</h5>
+                            //         </div>
+                            //         <button
+                            //             className="btn btn-info my-2"
+                            //             onClick={() => handleDetail(food._id)}
+                            //         >
+                            //             detalle
+                            //         </button>
+                            //         <button
+                            //             className="btn btn-danger my-2"
+                            //             onClick={() => handleDelet(user.id)}
+                            //         >
+                            //             eliminar
+                            //         </button>
+                            //     </div>
+                            // </div>
                         ))}
                     </div>
                 </div>
