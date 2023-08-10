e4b2fd24dea39b4f1a125f970a1a5360bd1ed830
Mario-Chavez <marioachavez1@gmail.com>
2023-08-10 11:46:11 -0300

    se egrego las variables de entornos

--- src/api/apiSlice.js
+++ src/api/apiSlice.js
@@ -1,11 +1,12 @@
 // Need to use the React-specific entry point to import createApi
 import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
+const URL_FOODS = import.meta.env.VITE_API_PLATOS;
 
 //backend-restfood.vercel.app/apiRestFood/platos
 export const foodApi = createApi({
     reducerPath: "foodApi",
     baseQuery: fetchBaseQuery({
-        baseUrl: "https://backend-restfood.vercel.app/apiRestFood",
+        baseUrl: URL_FOODS,
     }),
     endpoints: (builder) => ({
         // get all foods
