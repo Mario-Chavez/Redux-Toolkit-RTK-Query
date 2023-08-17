c5eac5cfe30c209867176d75b241a0177584b173
Mario-Chavez <marioachavez1@gmail.com>
2023-08-17 10:21:28 -0300

    se agrego logos de clients

--- src/components/Customers.jsx
+++ src/components/Customers.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { Container } from "react-bootstrap";
+import { Col, Container, Row } from "react-bootstrap";
 
 const Customers = () => {
     return (
@@ -7,11 +7,38 @@ const Customers = () => {
             style={{
                 backgroundColor: "#212529",
             }}
-            className="text-bg-dark"
+            className="text-bg-dark p-4"
         >
-            {" "}
-            <Container>
-                <h1>spy clientes</h1>
+            <Container className="text-center py-5">
+                <Row>
+                    <Col
+                        xs={12}
+                        md={4}
+                        className="border border-2 border-top-0 border-bottom-0"
+                    >
+                        <span className="border border-2 border-end-0 border-start-0 py-5">
+                            xs=12 md=8
+                        </span>
+                    </Col>
+                    <Col
+                        xs={12}
+                        md={4}
+                        className="border border-2 border-top-0 border-bottom-0"
+                    >
+                        <span className="border border-2 border-end-0 border-start-0 py-5">
+                            xs=12 md=8
+                        </span>
+                    </Col>
+                    <Col
+                        xs={12}
+                        md={4}
+                        className="border border-2 border-top-0 border-bottom-0"
+                    >
+                        <span className="border border-2 border-end-0 border-start-0 py-5">
+                            xs=12 md=8
+                        </span>
+                    </Col>
+                </Row>
             </Container>
         </div>
     );
