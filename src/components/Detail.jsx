f140358d278886ac62e5b4117f553ebad4492a2d
Mario-Chavez <marioachavez1@gmail.com>
2023-08-18 11:11:54 -0300

    cambio de estilos en el elemeto detalle

--- src/components/Detail.jsx
+++ src/components/Detail.jsx
@@ -2,14 +2,14 @@ import React from "react";
 import photo from "../assets/img-foto.jpg";
 import { useParams } from "react-router-dom";
 import { useGetOneFoodQuery } from "../api/apiSlice";
-import { Card, Col, Container, Image } from "react-bootstrap";
+import { Card, Col, Container, Image, Row } from "react-bootstrap";
 import { FallingLines } from "react-loader-spinner";
 
 const Detail = () => {
     const params = useParams();
 
     const { data: food, isLoading, isError } = useGetOneFoodQuery(params.id);
-    console.log(food);
+
     return (
         <>
             {isLoading ? (
@@ -20,28 +20,42 @@ const Detail = () => {
                     ariaLabel="falling-lines-loading"
                 />
             ) : (
-                <Container className="my-5">
-                    <div className="  my-5">
+                <div className="container-detail ">
+                    <Container className="mt-5 d-flex justify-content-center align-items-center">
+                        <Card className="m-5" style={{}}>
+                            <Row>
+                                <Col md={8}>
+                                    {" "}
+                                    {/* Columna izquierda */}
+                                    <Card.Body>
+                                        <Card.Title>{food.nombre}</Card.Title>
+                                        <Card.Text>
+                                            Some quick example text to build on the card
+                                            title and make up the bulk of the card's
+                                            content.
+                                        </Card.Text>
+                                    </Card.Body>
+                                </Col>
+                                <Col
+                                    md={4}
+                                    className="d-flex justify-content-center align-items-center"
+                                >
+                                    {" "}
+                                    {/* Columna derecha */}
+                                    <Card.Img variant="top" src={food.imagen} />
+                                </Col>
+                            </Row>
+                        </Card>
+                        {/* <div className="  my-5">
                         <div className="w-50 mx-auto">
                             <Image src={food.imagen} className="img-detail" />
                         </div>
 
                         <div>{food.nombre}</div>
                         <div>{food.categoria}</div>
-                    </div>
-                </Container>
-
-                // <Card border="primary" style={{ width: "18rem" }}>
-                //     <Card.Header>{food.nombre}</Card.Header>
-                //     <Card.Img variant="top" src={food.imagen} />
-                //     <Card.Body>
-                //         <Card.Title>{food.nombre}</Card.Title>
-                //         <Card.Text>
-                //             Some quick example text to build on the card title and make up
-                //             the bulk of the card's content.
-                //         </Card.Text>
-                //     </Card.Body>
-                // </Card>
+                    </div> */}
+                    </Container>
+                </div>
             )}
         </>
     );
