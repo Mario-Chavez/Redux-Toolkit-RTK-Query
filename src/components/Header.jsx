8e372992ee891770055e28e2ff6d98604900c2a5
Mario-Chavez <marioachavez1@gmail.com>
2023-08-23 10:19:43 -0300

    se realizo la primera parte de la page

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,12 +1,14 @@
 import React from "react";
 import { Navbar, Nav, Container } from "react-bootstrap";
-import WhatsAppBotton from "./WhatsAppBotton";
+import { Link } from "react-router-dom";
 
 const Header = () => {
     return (
         <Navbar expand="lg" className="navbar navbar-dark bg-dark fixed-top main-header ">
             <Container fluid className=" text-bg-dark">
-                <Navbar.Brand href="#">Food delivery</Navbar.Brand>
+                <Navbar.Brand as={Link} to="/">
+                    Food delivery
+                </Navbar.Brand>
                 <Navbar.Toggle aria-controls="navbarScroll" />
                 <Navbar.Collapse id="navbarScroll">
                     <Nav
@@ -14,8 +16,10 @@ const Header = () => {
                         style={{ maxHeight: "100px" }}
                         navbarScroll
                     >
-                        <Nav.Link href="#action1">Home</Nav.Link>
-                        <Nav.Link href="#action2">Link</Nav.Link>
+                        <Nav.Link as={Link} to="/">
+                            Home
+                        </Nav.Link>
+                        <Nav.Link href="#abautMe">Link</Nav.Link>
                     </Nav>
                 </Navbar.Collapse>
             </Container>
