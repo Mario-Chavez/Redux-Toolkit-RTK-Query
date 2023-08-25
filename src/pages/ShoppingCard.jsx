1bd9729b5ae7a54865f5965fcc45234d1e51c637
Mario-Chavez <marioachavez1@gmail.com>
2023-08-25 11:58:38 -0300

    se comenzo a realizar el compolnente del carrito

--- src/pages/ShoppingCard.jsx
+++ src/pages/ShoppingCard.jsx
@@ -1,13 +1,17 @@
 import React from "react";
+import { Col, Container, Row } from "react-bootstrap";
 
 const ShoppingCard = () => {
     return (
-        <div>
-            <h1>soy el carrito</h1>
-            <h1>soy el carrito</h1>
-            <h1>soy el carrito</h1>
-            <h1>soy el carrito</h1>
-            <h1>soy el carrito</h1>
+        <div className="mt-5 container-detail text-white ">
+            <h1>Gracias racias por tu compra</h1>
+            <Container>
+                <Row>
+                    <Col></Col>
+                    <Col></Col>
+                    <Col></Col>
+                </Row>
+            </Container>
         </div>
     );
 };
